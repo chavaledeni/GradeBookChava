c18169116176bafc5423e067ee56ec2bccedbe39
nikola.cavara <nikola.cavara@fitek.com>
2022-12-15 08:56:45 +0100

    novi kod

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Chava!");
+            var x = 34.1;
+            var y = 10;
+            var result = x + y;
+            Console.WriteLine(result);
+
+            if (args.Length > 0)
+            {
+            Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else 
+            {
+                Console.WriteLine("Hello");
+            }
         }
     }
 }
